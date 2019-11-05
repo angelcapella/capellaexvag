babf9875739801bfd1332d1dfd1c59cf9661e7e1
angelcapella <44397399+angelcapella@users.noreply.github.com>
2019-11-05 11:23:57 +0100

    Add files via upload

---
+++ provision1.sh
@@ -0,0 +1,13 @@
+apt-get -y update
+apt-get install -y git
+apt-get install -y apache2
+
+sudo apt-get upgrade
+sudo apt-get install vim
+sudo apt-get install apache2
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install php5-cgi php5-cli php5-curl php5-common php5-gd php5-mysql
+sudo service apache2 restart
